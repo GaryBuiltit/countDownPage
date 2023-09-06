48c23616447fb36625858c5fd9b5b595d01db4c8
Gary <alfordg15@gmail.com>
2023-09-05 23:28:21 -0400

    updates

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -1,2 +1,32 @@
 import React from "react";
+import BgSection from "./BgSection";
+import LogoUpload from "./logoUpload";
 
+const CreatePageForm = ({bgColorHandler, bgPicHandler, bgTypeHandler, logoHandler, headlineHandler, taglineHandler})=>{
+
+    return (
+        <form id="new-page-form" className="flex flex-col border-solid border-2 py-6 px-16 rounded-md space-y-4 bg-white shadow-lg">
+           
+            <LogoUpload logoHandler={logoHandler}/>
+            <div id="headline-section" className="space-y-2 flex flex-col">
+            <label htmlFor="headline" className="font-bold pb-2">Headline:</label>
+                <input type="text" id="headline" name="headline" onChange={hl=>{
+                    headlineHandler(hl.target.value)
+                }} className="border rounded"/>
+            </div>
+            <div id="tagline-section" className="space-y-2 flex flex-col">
+            <label htmlFor="tagline" className="font-bold pb-2">Tagline:</label>
+                <textarea id="tagline" name="tagline" onChange={tl=>{
+                    taglineHandler(tl.target.value);
+                }} className="border rounded"/>
+            </div>
+            <BgSection bgTypeHandler={bgTypeHandler} bgColorHandler={bgColorHandler} bgPicHandler={bgPicHandler}/>
+            <div id="deadline-section" className="flex flex-col">
+            <label htmlFor="date-time" className="font-bold pb-2">Deadline:</label>
+                <input id="date-time" name="date-time" type="datetime-local"/>
+            </div>
+            </form>
+    )
+}
+
+export default CreatePageForm;
--- src/routes/CreateCountDown.jsx
+++ src/routes/CreateCountDown.jsx
@@ -1,14 +1,16 @@
-import React, {useState} from "react";
+import React, {useState, createContext } from "react";
 import "../main.css";
 import Preview from "../components/Preview";
-import BgSection from "../components/BgSection";
-
+import CreatePageForm from "../components/CreateForm"
 
 
 const Create = ()=>{
-    const [BgState, setBgState] = useState("color")
-    const [bgColor, setBgColor] = useState("")
-    const [bgPic, setPic] = useState("")
+    const [BgState, setBgState] = useState("color");
+    const [bgColor, setBgColor] = useState("");
+    const [bgPic, setPic] = useState("");
+    const [logo, setLogo] = useState("https://firebasestorage.googleapis.com/v0/b/countdown-page-c3e5f.appspot.com/o/bgimages%2Flogoipsum-297.svg?alt=media&token=d64ef49c-2029-4374-814c-dbf74396e22dgs://countdown-page-c3e5f.appspot.com/bgimages/logoipsum-297.svg");
+    const [headline, setHeadline] = useState("HeadLine Text Hear");
+    const [tagline, setTagline] = useState("This is where you tagline goes! Use this section to add whatever text you want to talk more about what you have going on.");
     
     const bgTypeHandler = (bgType)=>{
         setBgState(bgType);
@@ -22,28 +24,31 @@ const Create = ()=>{
         setPic(bgPic);
     }
 
+    const logoHandler = (logo)=>{
+        setLogo(logo);
+    }
+
+    const headlineHandler = (headline)=>{
+        setHeadline(headline);
+    }
+
+    const taglineHandler = (tagline)=>{
+        setTagline(tagline);
+    }
+
+
     const previewHandler = ()=>{
         if (BgState === "color") {
-            return <Preview type={BgState} color={bgColor}/>
+            return <Preview type={BgState} color={bgColor} logo={logo} headline={headline} tagline={tagline}/>
         } else if(BgState === "image"){
-            return <Preview type={BgState} pic= {bgPic}/>
+            return <Preview type={BgState} pic= {bgPic} logo={logo}  headline={headline} tagline={tagline}/>
         }
     }
-    
 
     return (
         <div className="flex flex-row items-center justify-evenly h-full space-x-3">
-        <div className="flex items-center">
-        <form id="new-page-form" className="flex flex-col border-solid border-2 py-6 px-16 rounded-md space-y-4 bg-white shadow-lg">
-            <div id="deadline-section" className="flex flex-col">
-            <label htmlFor="date-time" className="font-bold pb-2">Deadline:</label>
-                <input id="date-time" name="date-time" type="datetime-local" className=""/>
-            </div>
-            <BgSection bgTypeHandler={bgTypeHandler} bgColorHandler={bgColorHandler} bgPicHandler={bgPicHandler}/>
-            </form>
-        </div>
+            <CreatePageForm  bgTypeHandler={bgTypeHandler} bgColorHandler={bgColorHandler} bgPicHandler={bgPicHandler} logoHandler={logoHandler} taglineHandler={taglineHandler} headlineHandler={headlineHandler}/>
             {previewHandler()}
-
         </div>
     )
 }
