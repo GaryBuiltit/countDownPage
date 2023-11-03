89ec7bab69084bb251f5b95185f24276f75468bb
Gary <alfordg15@gmail.com>
2023-11-03 17:05:13 -0400

    deploy fixes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {createRoot} from "react-dom/client"
 import { StrictMode } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createHashRouter, RouterProvider } from "react-router-dom";
 import DashBoard from "./routes/Dashboard";
 import Create from "./routes/CreateCountDown";
 import Pages from "./routes/Pages";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <DashBoard/>,
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-<RouterProvider router={router} history="hashHistory" />
+<RouterProvider router={router}/>
   </StrictMode>
 )
 
